0280b763d115ba05d994117c796dfd00da4921b7
WhiteDopeOnPunk <24621027+WhiteDopeOnPunk@users.noreply.github.com>
2022-05-10 19:29:26 -0400

    it's alive!

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+
+TOKEN = open('token', 'r').read()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+
+    if message.author == client.user:
+        return 
+    
+    if message.channel.name == 'shitposting':
+        if user_message.lower()[-1] == '?':
+            await message.channel.send('what was the question?')
+            return
+
+client.run(TOKEN)
